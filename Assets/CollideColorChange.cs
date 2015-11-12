58c55e9035f5c75870627e49947b0520e5f46f3d
unknown <tomguz@o2.pl>
2015-11-12 12:35:32 +0100

    z laborek

---
+++ Assets/CollideColorChange.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollideColorChange : MonoBehaviour {
+
+    bool setBlue = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+
+    void OnTriggerEnter()
+    {
+        if (!setBlue)
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.blue;
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.red;
+        }
+        setBlue = !setBlue;
+    }
+}
